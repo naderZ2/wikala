8403a1873697221babb3bfcfcc6c28b720d1958b
naderZ2 <{ID}+{username}@users.noreply.github.com>
2025-10-05 18:15:48 +0300

    add ad from admin

--- app/Http/Requests/Admin/Ads/StoreRequest.php
+++ app/Http/Requests/Admin/Ads/StoreRequest.php
@@ -26,6 +26,8 @@ class StoreRequest extends FormRequest
             'user_id' => 'required|exists:users,id',
             'type_id' => 'required|exists:ads_type,id',
             // 'ad_number' => 'required|string|unique:ads,ad_number',
+            'city_id'        => 'required|exists:cities,id',
+            'region_id'      => 'required|exists:cities,id',
             'title' => 'required|string|max:255',
             'description' => 'required|string',
             'contact_method' => 'nullable|in:phone,chat,email',
