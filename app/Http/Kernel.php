f1202cd106ada70fda738412ba07298baa42b96b
naderZ2 <{ID}+{username}@users.noreply.github.com>
2026-04-07 20:02:11 +0200

    Employee system

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,8 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkLanguage' => \App\Http\Middleware\CheckLanguage::class,
         'CheckUserActiviation' => \App\Http\Middleware\CheckUserActiviation::class,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
     ];
 }
