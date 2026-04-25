79373109e35c81c8c99e2ebb44fdf71253d83498
naderZ2 <{ID}+{username}@users.noreply.github.com>
2026-04-25 20:07:58 +0300

    video in baner

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -4,11 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Traits\FileUploadTrait;
 
 class Slider extends Model
 {
-    use FileUploadTrait;
     use HasFactory;
     protected $fillable = [
         'name', 'link', 'type', 'video'
@@ -18,27 +16,5 @@ class Slider extends Model
         'updated_at',
         'created_at',
     ];
-
-    public function setNameAttribute($value)
-    {
-        if ($value && is_object($value)) {
-            $mime = $value->getMimeType();
-            $ext  = strtolower($value->getClientOriginalExtension());
-
-            if (str_starts_with($mime, 'video/')) {
-                // Video file: store in 'video' column, clear 'name'
-                $this->attributes['type']  = 'video';
-                $this->attributes['video'] = $this->uploadFile($value, 'sliders', $this->attributes['video'] ?? "");
-                $this->attributes['name']  = null;
-                return;
-            } elseif ($ext === 'gif' || $mime === 'image/gif') {
-                $this->attributes['type'] = 'gif';
-            } else {
-                $this->attributes['type'] = 'image';
-            }
-
-            $this->attributes['name'] = $this->uploadFile($value, 'sliders', $this->attributes['name'] ?? "");
-        }
-    }
 
 }
