95c9d29c8eab1e023acd4924677b4fb829976b31
naderZ2 <{ID}+{username}@users.noreply.github.com>
2026-04-25 20:07:17 +0300

    video in baner

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -6,37 +6,88 @@ use App\Models\Slider;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\File;
+use App\Traits\FileUploadTrait;
 use App\Http\Requests\Admin\Banner\{StoreRequest,EditRequest};
 
 class SliderController extends Controller
 {
+    use FileUploadTrait;
+
     public function index(){
         $sliders  =Slider::get();
         return view('admin.slider.index',compact('sliders'));
     }
 
     public function store(StoreRequest $request){
-        Slider::create($request->validated());
+        $data = $request->validated();
+        $file = $request->file('name');
+
+        if ($file) {
+            $mime = $file->getMimeType();
+            $ext  = strtolower($file->getClientOriginalExtension());
+
+            if (str_starts_with($mime, 'video/')) {
+                $data['type']  = 'video';
+                $data['video'] = $this->uploadFile($file, 'sliders');
+                $data['name']  = '';
+            } elseif ($ext === 'gif' || $mime === 'image/gif') {
+                $data['type'] = 'gif';
+                $data['name'] = $this->uploadFile($file, 'sliders');
+            } else {
+                $data['type'] = 'image';
+                $data['name'] = $this->uploadFile($file, 'sliders');
+            }
+        }
+
+        Slider::create($data);
         return  to_route('slider.index')->with('success',trans('lang.created')); 
     }
     
     public function update(EditRequest $request){
-        $slider=Slider::find($request->id);
-        $slider->update($request->validated());
+        $slider = Slider::find($request->id);
+        $data   = $request->validated();
+        $file   = $request->file('name');
+
+        if ($file) {
+            $mime = $file->getMimeType();
+            $ext  = strtolower($file->getClientOriginalExtension());
+
+            if (str_starts_with($mime, 'video/')) {
+                // Delete old files
+                if ($slider->name) File::delete(public_path($slider->name));
+                if ($slider->video) File::delete(public_path($slider->video));
+
+                $data['type']  = 'video';
+                $data['video'] = $this->uploadFile($file, 'sliders');
+                $data['name']  = '';
+            } elseif ($ext === 'gif' || $mime === 'image/gif') {
+                if ($slider->name) File::delete(public_path($slider->name));
+                if ($slider->video) File::delete(public_path($slider->video));
+
+                $data['type']  = 'gif';
+                $data['name']  = $this->uploadFile($file, 'sliders');
+                $data['video'] = null;
+            } else {
+                if ($slider->name) File::delete(public_path($slider->name));
+                if ($slider->video) File::delete(public_path($slider->video));
+
+                $data['type']  = 'image';
+                $data['name']  = $this->uploadFile($file, 'sliders');
+                $data['video'] = null;
+            }
+        }
+
+        $slider->update($data);
         return  to_route('slider.index')->with('success',trans('lang.updated')); 
     }
 
     public function destroy(Request $request){
         $slider=Slider::find($request->id);
-        // Delete image file
         if ($slider->name) {
-            $img = public_path($slider->name);
-            File::delete($img);
+            File::delete(public_path($slider->name));
         }
-        // Delete video file
         if ($slider->video) {
-            $vid = public_path($slider->video);
-            File::delete($vid);
+            File::delete(public_path($slider->video));
         }
         $slider->delete();
         return  to_route('slider.index')->with('success',trans('lang.deleted')); 
